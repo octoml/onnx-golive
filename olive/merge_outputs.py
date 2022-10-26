c1a7dc3de01af272709098e3104eccb9491b7480
Ubuntu <ubuntu@ip-172-31-48-36.us-west-2.compute.internal>
2022-10-26 17:07:29 +0000

    Add merge script

--- olive/merge_outputs.py
+++ olive/merge_outputs.py
@@ -1,16 +1,20 @@
 import logging
 import os
 import sys
-
 import pandas as pd
+from ec2_metadata import ec2_metadata
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 def get_aws_instance_type():
     return ec2_metadata.instance_type
+
 def write_csv_summary(result_dir):
     all_dfs = []
+    instance_type = get_aws_instance_type() or "UNKNOWN"
+
+    logger.info(f"Summarizing results from {result_dir}")
 
     for sub_dir in os.listdir(result_dir):
         result_path = os.path.join(result_dir, sub_dir, 'olive_result.csv')
@@ -18,7 +22,6 @@ def write_csv_summary(result_dir):
             logger.info("Skipping non-existing result from: " + result_path)
             continue
 
-        logger.info("Reading results from: " + result_path)
         with open(result_path) as fh:
             df = pd.read_csv(fh)
             num_columns = len(df.columns)
@@ -29,6 +32,8 @@ def write_csv_summary(result_dir):
                 all_dfs.append(df)
 
     combined_csv = pd.concat(all_dfs)
+    combined_csv.insert(0, "instance_type", instance_type)
+
     out_file = os.path.join(result_dir, 'merged.csv')
 
     logger.info("Writing results to: " + out_file)
--- olive/preprocess.py
+++ olive/preprocess.py
@@ -13,11 +13,9 @@ from onnxmltools.utils import load_model, save_model
 import onnxruntime as ort
 from onnxruntime.tools import onnx_model_utils
 
-import pandas as pd
-from ec2_metadata import ec2_metadata
-
 from olive.optimization_config import OptimizationConfig
 from olive.optimize import optimize
+from olive.merge_outputs import write_csv_summary
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -92,36 +90,6 @@ def get_configurations(model_config_dict):
     for func_list, name_list in zip(all_funcs, all_names):
         yield (func_list, name_list)
 
-def get_aws_instance_type():
-    return ec2_metadata.instance_type
-
-def write_csv_summary(result_dir):
-    all_dfs = []
-    instance_type = get_aws_instance_type() or "UNKNOWN"
-
-    for sub_dir in os.listdir(result_dir):
-        result_path = os.path.join(result_dir, sub_dir, 'olive_result.csv')
-        if not os.path.exists(result_path):
-            logger.info("Skipping non-existing result from: " + result_path)
-            continue
-
-        logger.info("Reading results from: " + result_path)
-        with open(result_path) as fh:
-            df = pd.read_csv(fh)
-            num_columns = len(df.columns)
-            for idx, dimension in enumerate(sub_dir.split('|')):
-                
-                dim_name, dim_value = dimension.split('=')
-                df.insert(num_columns + idx, dim_name, dim_value)
-                all_dfs.append(df)
-
-    combined_csv = pd.concat(all_dfs)
-    combined_csv.insert(0, "instance_type", instance_type)
-
-    out_file = os.path.join(result_dir, 'merged.csv')
-
-    logger.info("Writing results to: " + out_file)
-    combined_csv.to_csv(out_file, header=True, index=False,  encoding='utf-8')
 
 def load_optimization_config(model_path, olive_config_dict):
     opt_config = OptimizationConfig(
