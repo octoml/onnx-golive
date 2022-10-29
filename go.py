f80e250d3592c041ee29074c08341e9320a7c740
Andrew Whitaker <awhitaker@octoml.ai>
2022-10-28 22:34:49 -0700

    Config fixes

--- go.py
+++ go.py
@@ -1,7 +1,10 @@
+
 import argparse
 import os
 import sys
 import subprocess
+
+from pathlib import Path
 
 def run1(c, cwd=None):
     if isinstance(c, list):
@@ -12,22 +15,23 @@ def run1(c, cwd=None):
         subprocess.check_call(c, shell=True, cwd=cwd)
 
 def go(gpu, argv):
-    run1("sudo snap install docker")
-
-    if not os.path.exists('onnx-golive'):
-        run1("git clone https://github.com/octoml/onnx-golive.git")
+#    run1("sudo snap install docker")
 
     dockerfile = 'Dockerfile.cpu' if not gpu else 'Dockerfile.trt'
     target = 'octoml/olive:cpu' if not gpu else 'octoml/olive:trt'
 
-    run1(f"sudo docker build -t {target} -f {dockerfile} .", cwd='onnx-golive')
+    run1(f"sudo docker build -t {target} -f {dockerfile} .")
 
+    home = str(Path.home())
+    args = ['docker', 'run', '-it', '--rm', '--volume', f'{home}:/tmp']
+
+    if gpu:
+        args.extend(['--gpus', 'all'])
+    
     aws_keys = ['AWS_ACCESS_KEY_ID', 'AWS_SESSION_TOKEN','AWS_SECRET_ACCESS_KEY']
-
-    args = ['sudo', f"--preserve-env={','.join(aws_keys)}",
-            'docker', 'run', '-it', '--rm', '--volume', '/home/ubuntu:/tmp']
     for key in aws_keys:
         args.extend(['--env', key])
+
     args.append(target)
     args.extend(argv)
     run1(args)
