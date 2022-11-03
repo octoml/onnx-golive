6dc2ec9c2ca6090ab516661f2d2a887652587f25
Gaz Iqbal <giqbal@octoml.ai>
2022-11-03 16:50:01 +0000

    enabled run1 to support inputs other than fp32

--- run1.py
+++ run1.py
@@ -1,48 +1,48 @@
 import sys
-from typing import Dict
-
 import time
+
 import numpy as np
-import onnxruntime as ort
 import onnx
+import onnxruntime as ort
+
+from olive.constants import ONNX_TO_NP_TYPE_MAP
+
+WARMUP_COUNT = 1
+BENCHMARK_COUNT = 10
 
 def run(
     onnx_model: onnx.ModelProto,
-    input_values: Dict[str, np.ndarray],
     ep: str,
-    repeats=20,
+    warmup_count=WARMUP_COUNT,
+    benchmark_count=BENCHMARK_COUNT,
 ) -> float:
-    session = ort.InferenceSession(onnx_model.SerializeToString(), providers=[ep])
 
-    session.run(None, input_values)
+    session_options = ort.SessionOptions()
+    # session_options.log_severity_level = 2
+    # session_options.log_verbosity_level = 0
+    # session_options.intra_op_num_threads = 8
+    session = ort.InferenceSession(onnx_model.SerializeToString(), session_options, providers=[ep])
+    print(f"SELECTED EPs: {session.get_providers()}")
+
+    # Prepare inputs
+    inputs = session.get_inputs()
+    input_names, input_types, input_dims = zip(*[(i.name, ONNX_TO_NP_TYPE_MAP[i.type], i.shape) for i in inputs])
+    print(f"INPUT NAMES: {input_names}, TYPES: {input_types}, SHAPES: {input_dims}")
+
+    input_values = {}
+    for i in range(0, len(inputs)):
+        # regard unk__32 and None as 1
+        shape = [1 if (x is None or (type(x) is str)) else x for x in input_dims[i]]
+        vals = np.random.random_sample(shape).astype(input_types[i])
+        input_values[input_names[i]] = vals
+
+    for _ in range(warmup_count):
+        session.run(None, input_values)
 
     st = time.time()
-    for k in range(repeats):
+    for _ in range(benchmark_count):
         session.run(None, input_values)
-    return (time.time() - st) / repeats
-
-def get_input_shapes(onnx_model: onnx.ModelProto):
-    shapes = {}
-    for input in onnx_model.graph.input:
-        # get type of input tensor
-        name = input.name
-        tensor_type = input.type.tensor_type
-
-        # check if it has a shape:
-        assert tensor_type.HasField("shape")
-
-        shape = []
-        for d in tensor_type.shape.dim:
-            # the dimension may have a definite (integer) value or a symbolic identifier or neither:
-            if (d.HasField("dim_value")):
-                shape.append(d.dim_value)
-            elif (d.HasField("dim_param")):
-                shape.append(d.dim_param)
-            else:
-                shape.append("?")
-        shapes[name] = shape
-
-    return shapes
+    return ((time.time() - st) / benchmark_count, benchmark_count)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -50,11 +50,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     onnx_model = onnx.load(sys.argv[1])
-    ep = TensorrtExecutionProvider = sys.argv[2] if len(sys.argv) >= 3 else 'TensorrtExecutionProvider'
+    available_eps = ort.get_available_providers()
+    print(f"Available EPs: {available_eps}")
+    ep = sys.argv[2] if len(sys.argv) >= 3 else 'TensorrtExecutionProvider'
 
-    input_shapes = get_input_shapes(onnx_model)
-    print(f"INPUTS: {input_shapes}")
-
-    input_dict = {shape_name: np.zeros(shape_size, dtype='float32') for (shape_name, shape_size) in input_shapes.items()}
-    latency = run(onnx_model, input_dict, ep)
-    print(f"Avg latency: {latency * 1000} ms")
+    latency, runs = run(onnx_model, ep)
+    print(f"Avg latency: {latency * 1000:0.2f} ms, {runs} runs")
